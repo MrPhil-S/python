cc677975ba1aa5c9ca94d9c32d04c63d4e3b93c7
TheThirstyPlebeian <noTheThirstyPlebeia@ex.com>
2020-08-01 15:41:41 -0700

    More practice

--- inspecting_data.py
+++ inspecting_data.py
@@ -1,36 +1,59 @@
 import pandas as pd
 
+#df = pd.read_csv ('..\git\python_pandas\sample_data\\02 Introduction to Pandas\intel.csv')
 df = pd.read_csv ('..\git\python_pandas\sample_data\\02 Introduction to Pandas\intel.csv')
 #df is a variable
 
-# print(df)
+#print(df)
 
 # How to check data type
 # print(type(df))
+#print (type(df))
 
-# print(df.shape)
+#print(df.shape)
 
 # column names
-# print(df.columns)
+#print(df.columns)
 
 # limit data result to first x  number of rows (5 by default)
-# print(df.head(100))
+#print(df.head())
 
-# limit data result to last x  number of rows (5 by default)
-# print(df.tail(100))
+#print("limit data result to last x  number of rows (5 by default)")
+#print(df.tail())
 
 # data info
-# print(df.info())
+#print(df.info())
 
-# print the "Open" col  (case sensitive to the data header)
+# print the "Open" col  (case sensitive to the data header) - in a panda series
+# [] brings us into the data frama
 # open = df['Open']
 # print(open)
-
+#or
+#high = df['High']
+#print(high)
+#open = df['Open']
 #print(open.head())
+
 
 #View 1+ columns side by side
 # [] brings us into the data frama
 #print(df[['Open','Close']].head(50))
+#print(df[['Open','Close']].head())
 
 #avg, min, max, %25tile
-# print(df.describe())
+#print(df.describe())
+print(df.describe())
+
+###COONDITIONAL FILTERING####
+#my_open = df['Open'] > 40
+#print(my_open)
+#print(df[my_open])
+#print(df[df['Open'] > 55])
+
+##using NumpPy and pandas together###
+#open = df['Open']
+#what type of data are we using
+#print(type(open))
+#new_open = open.values
+#print(type(new_open))
+#print(new_open)
---
+++ s2_6_dataframe_from_dictionary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+course_sales = {'course':['Python','Ruby','Excel','C++'],
+                'day':['Mon','Tue','Wed','Tue'],
+                'price':[5,18,15,20],
+                'sale':[2,3,5,7]}
+
+#print(course_sales)
+#convert to DataFrame
+
+#df_sales = pd.DataFrame(course_sales)
+#print(df_sales)
+
+course  =  ['Python','Ruby','Excel','C++']
+day = ['Mon','Tues','Wed','Tues']
+price = [5,13,15,17]
+sale = [2,3,5,7]
