ddb2dfd150c9313254b3749b642bd605b8ac2158
MrPhil-S <pschims@gmail.com>
2022-06-20 20:23:54 -0700

    __main__ stuff

---
+++ main_module1.py
@@ -0,0 +1,13 @@
+print (f"First module's name: {__name__}")
+
+#__name__ is a predefined variable when python runs
+
+def main():
+    print (f"First module's name: {__name__}")
+
+
+#is this file being run directly and not being
+#imported by another file?
+
+if __name__ == '__main__':
+    main()
---
+++ main_module2.py
@@ -0,0 +1,3 @@
+import main_module1
+
+print (f"Second module's name: {__name__}")
