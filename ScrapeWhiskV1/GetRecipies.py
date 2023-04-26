ce91c95b7c3a1af8725b075132f4cfe3cca358a6
MrPhil-S <pschims@gmail.com>
2023-04-26 00:47:19 -0700

    Consolidated loop process and added source URLs to DB

--- ScrapeWhiskV1/GetRecipies.py
+++ ScrapeWhiskV1/GetRecipies.py
@@ -21,7 +21,10 @@ cursor.execute('DROP TABLE IF EXISTS recipe')
 #cursor.execute('CREATE TABLE recipe (id TEXT PRIMARY KEY, title TEXT)')
 cursor.execute('''CREATE TABLE recipe (
       title TEXT,
-      url TEXT)''')
+      whisk_url TEXT,
+      source_url TEXT,
+      ingredient_count INT, 
+      total_time TEXT)''')
 
 # query the database
 cursor.execute('SELECT * FROM recipe')
@@ -74,68 +77,115 @@ while x > 1:
 print(f'{x} seconds left, continuing...')
 
 #scroll to bottom of cards
-last_height = driver.execute_script("return document.body.scrollHeight")
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(2)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-
-# get all the cards' titles
-card_titles = []
-cards = driver.find_elements(By.XPATH, '//h3[ contains(@class, "s1279")]')
-for card in cards:
-  card_titles.append(card.text)
-#card_titles = card_titles[:3]
+#last_height = driver.execute_script("return document.body.scrollHeight")
+#while True:
+#    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#    sleep(2)
+#    new_height = driver.execute_script("return document.body.scrollHeight")
+#    if new_height == last_height:
+#        break
+#    last_height = new_height
 
 #get Whisk recipe page URLs
 card_urls = []
-urls = driver.find_elements(By.XPATH, '//a[ contains(@class, "s189")]')  
-for url in urls:
-  url = url.get_attribute("href")
+card_headers_objs = driver.find_elements(By.XPATH, '//a[ contains(@class, "s189")]')  
+for card_headers_obj in card_headers_objs:
+  card_headers = card_headers_obj.text
+  # print(f'HEADERS: {card_headers}')
+
+  title = card_headers.split('\n')[0]
+  
+  try:
+    ingredient_count = card_headers.split('\n')[1]
+    if "ingredients" in ingredient_count:
+      ingredient_count = ingredient_count.replace('ingredients','')
+    else:
+      ingredient_count = None
+  except:
+    ingredient_count = None
+  
+  
+  try:
+    total_time = card_headers.split('\n')[2]
+    matches = ['h ', "min"]
+    if any([x in total_time for x in matches]):
+      pass
+    else:
+      total_time = None
+  except:
+    total_time = None
+
+  url = card_headers_obj.get_attribute("href")
   url =  url.split('?')[0]
   card_urls.append(url)
-  #print(url)
+
+  data = [title, url, ingredient_count, total_time]
+  insert_statement = 'INSERT INTO recipe (title, whisk_url, ingredient_count, total_time) VALUES (?, ?, ?, ?)'
+  conn.execute(insert_statement, data)
+  
+conn.commit()
+
 total_recipes = len(card_urls)
 print(f'{total_recipes} URLs found ')
-#card_urls = card_urls[:3]
+#TESTING - limite the amount
+card_urls = card_urls[:30]
 
 #for each Whisk page, navigate within and get the souce URL
 source_url_count = 0
 source_urls = []
-for page in card_urls:
-  driver.get(page)
-  random_sleep = random.uniform(1.5, 5)
+for whisk_url in card_urls:
+  driver.get(whisk_url)
+  random_sleep = random.uniform(1.5, 3)
   sleep(random_sleep)
   find_href = driver.find_elements(By.XPATH, '//a[ contains(@class, "s321")]')  
-  for recipe_url in find_href:
-    recipe_url = recipe_url.get_attribute("href")
-    #print(recipe_url)
-    source_urls.append(recipe_url)
-  source_url_count += 1
-  print(f'Remaining source urls obtained: {total_recipes - source_url_count} of {total_recipes}, ({int((source_url_count/total_recipes)*100)}%)')
+  if len(find_href) > 0:
+    for source_url in find_href:
+      source_url = source_url.get_attribute("href")
+      source_url =  source_url.split('?')[0]
+      #some recipes do not have an extranal URL. If so, this will return the current URL
+      if source_url.startswith('https://my.whisk.com/profile/'):
+        source_url = driver.current_url
+      print(f'UPDATE recipe SET source_url = {source_url} WHERE whisk_url = {whisk_url}')
+      conn.execute('''UPDATE recipe SET source_url = ? WHERE whisk_url = ?''', (source_url, whisk_url))
+      conn.commit()
 
+      source_urls.append(source_url)
 
-if len(card_titles) == len(source_urls):
-  data = zip(card_titles, source_urls)
-  # insert the data into the table
-  insert_statement = 'INSERT INTO recipe (title, url) VALUES (?, ?)'
-  conn.executemany(insert_statement, data)
-  conn.commit()
-  conn.close()
-  print(f'{len(card_titles)} records inserted successfully')
-else:
-  print(f'lists do not match. Card titles: {len(card_titles)} and source urls: {len(source_urls)}')
-  print(card_titles)
-  print(source_urls)
+    source_url_count += 1
+    print(f'Remaining source urls obtained: {total_recipes - source_url_count} of {total_recipes}, ({int((source_url_count/total_recipes)*100)}%)')
+  else:
+    source_urls.append("Not Found")
+   
 
+  #times = driver.find_elements(By.XPATH, '//div[ @class="s12604"]/span[@class="s120 s1479"]')
+  #types = driver.find_elements(By.XPATH, '//*div[ @class="s12604"]')
+  types = driver.find_elements(By.XPATH, '//div[ @class="s12604"]')
+  times = driver.find_elements(By.XPATH, '//div[ @class="s120 s1479"]')
+  
+  #alltime = zip(types, times)
+  #print(alltime)
+  print(times)
 
-
-# print the card titles
-#for title in card_titles:
- # print(title)
-#  cursor.execute('insert into recipe (title) values (?)', (title,))
+  prep = []
+  cook = []
+  othertime = []
+  for type in types:
+    print(type.text)
+    for time in times:
+      
+      print(time.text)
+      if type.text =='Prep:':
+        prep.append(time.text)
+      elif type.text == 'Cook:':
+        cook.append(time.text)
+      else: 
+        othertime.append(type.text + ' ' + time.text)
+  print(prep)
+  print(cook)
+  print(othertime)
+  all = zip(prep, cook, othertime)
+  print(all)
+  
+conn.close()
 
 #driver.quit()
